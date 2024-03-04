2f040f70f25f6643ca3d66e607b28857467cead6
dasUnicorn <info@dasunicorn.de>
2024-03-04 14:57:49 +0100

    feat: add info page

---
+++ kladdera/src/components/Info.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Info() {
+	return (
+		<div>
+			<h1>About Us</h1>
+			<div>
+				<p>No person can reach their full potential every day.</p>
+			</div>
+			<section>
+				<h2>How Kladderadatsch Works</h2>
+				<p>For us, tasks involve effort and are individual to each person.
+				You can set your own energy levels or use predefined values. 
+				Depending on your energy level, we will suggest other tasks and set
+				an individual daily goal.
+				</p>
+			</section>
+			<section>
+				<h2>We do not care which task you choose.</h2>
+				<p>Instead of a list of tasks that quickly becomes overwhelming,
+				we show you a realistic selection of options.
+				If none of the tasks appeal to you, you can shuffle through several options.
+				</p>
+			</section>
+			<section>
+				<h2>We track your Tasks and Energy-Level</h2>
+				<p>Adjust your energy level whenever you want.
+				We visualize your changes over time to give you a good overview of your
+				own energy balance.
+				</p>
+			</section>
+		</div>
+	)
+}
+
+export default Info;
