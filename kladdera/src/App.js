d922430ec8e6ebc1c97566f652537afa1a69f08b
dasUnicorn <info@dasunicorn.de>
2024-03-10 23:50:40 +0100

    style: move padding styling from all to individual components

--- kladdera/src/App.js
+++ kladdera/src/App.js
@@ -8,9 +8,7 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-    <div>
-      <Navbar loggedIn={false}/>
-    </div>
+    <Navbar loggedIn={false}/>
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/info" element={<Info />}/>
