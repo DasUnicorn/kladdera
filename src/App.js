163c57891625103816330170029367a5bc9826bb
dasUnicorn <info@dasunicorn.de>
2024-04-03 01:13:34 +0200

    add functionality to signup

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Info from './components/Info';
 import Home from './components/Home';
 import Signup from './components/Signup';
 import SignInForm from "./pages/auth/SignInForm";
+import SignUpForm from "./pages/auth/SignUpForm";
 import { Routes, Route } from 'react-router-dom';
 import "./api/axiosDefaults";
 
@@ -15,7 +16,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="/info" element={<Info />}/>
-      <Route path="/signup" element={<Signup />}/>
+      <Route path="/signup" element={<SignUpForm />}/>
       <Route path="/login" element={<SignInForm />}/>
     </ Routes>
     </>
---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import axios from "axios";
+
+
+const SignUpForm = () => {
+  const [signUpData, setSignUpData] = useState({
+    email: "",
+    password1: "",
+    password2: "",
+  });
+  const { email, password1, password2 } = signUpData;
+
+  const [errors, setErrors] = useState({});
+
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setSignUpData({
+      ...signUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/register/", signUpData);
+      navigate('/signin');
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <>
+    <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div class="w-full bg-gold rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 class="text-xl font-bold leading-tight tracking-tight text-blue-dark md:text-2xl">
+                    Create An Account
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                      <div>
+                          <label for="email" class="block mb-2 text-sm font-medium text-blue-dark">Your email</label>
+                            <input type="email" 
+                            name="email" 
+                            id="email" 
+                            value={email} 
+                            onChange={handleChange}
+                            class="bg-gray-50 border border-blue-dark text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " 
+                            placeholder="name@domain.com" 
+                            required="" />
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-blue-dark">Password</label>
+                            <input type="password" 
+                            name="password1" 
+                            id="password1" 
+                            value={password1} 
+                            onChange={handleChange}
+                            placeholder="••••••••" 
+                            class="bg-gray-50 border border-blue-dark text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" 
+                            required="" />
+                        </div>
+                        <div>
+                            <label for="confirm-password" class="block mb-2 text-sm font-medium text-blue-dark">Confirm password</label>
+                            <input type="confirm-password" 
+                            name="password2" 
+                            id="password2" 
+                            value={password2} 
+                            onChange={handleChange}
+                            placeholder="••••••••" 
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" 
+                            required="" />
+                        </div>
+                        <button type="submit" class="w-full text-blue-dark bg-orange hover:bg-blue-light hover:text-gold focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
+                        <p class="text-sm font-light text-blue-dark">
+                        Already have an account? <Link to="/login" class="font-medium text-blue-dark text-bold hover:underline">Login here</Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    </>
+  );
+};
+
+export default SignUpForm;
