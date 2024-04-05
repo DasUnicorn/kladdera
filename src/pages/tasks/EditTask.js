f45f9e28e6cb52c41deff7e194e538e36dc9c5f3
dasUnicorn <info@dasunicorn.de>
2024-04-06 01:09:34 +0200

    fix spacing

--- src/pages/tasks/EditTask.js
+++ src/pages/tasks/EditTask.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../../api';
- import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const EditTask = () => {
  const { taskId } = useParams();
